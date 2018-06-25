96cdccba4570b799e29e93b58cbb82efb9d0f643
Prasanna <prasannaraajkumar@gmail.com>
2018-06-25 00:02:38 -0700

    webpack config added

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+  entry: [path.join(__dirname, './src/index.js')],
+  devtool: 'inline-source-map',
+  output: {
+    path: path.join(__dirname, 'build'),
+    publicPath: '/build/',
+    filename: 'bundle.js'
+  },
+  // devServer: {
+  //   contentBase: path.join(__dirname),
+  //   port: 9000,
+  //   compressed: true
+  // },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['babel-preset-react', 'babel-preset-es2015', 'babel-preset-stage-3']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+      }
+    ]
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
